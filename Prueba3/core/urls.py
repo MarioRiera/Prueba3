d8b07e36cc86f3f00987068e8e487b496b27aeba
MarioRiera <ma.riera@duocuc.cl>
2021-07-11 20:47:16 -0400

    agregando campos

--- Prueba3/core/urls.py
+++ Prueba3/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import inicio
+from .views import inicio, galeria, nosotros
 
 urlpatterns = [
     path('',inicio, name="index"),
+    path('galeria/',galeria, name="galeria"),
+    path('nosotros/',nosotros, name="nosotros"),
 ]
