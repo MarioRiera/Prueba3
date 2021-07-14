4a7d230d3ead4fa719ab5281372ec1f4db01a803
MarioRiera <ma.riera@duocuc.cl>
2021-07-13 20:21:49 -0400

    Se agrega base de datos

--- Prueba3/Prueba3/settings.py
+++ Prueba3/Prueba3/settings.py
@@ -79,8 +79,19 @@ WSGI_APPLICATION = 'Prueba3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': 'ejemplo',
+
+        'USER': 'Duoc',
+
+        'PASSWORD': 'duoc2021',
+
+        'HOST': 'localhost',
+
+        'PORT': '3306',
+
     }
 }
 
