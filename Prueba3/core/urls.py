4c60962823a3e36d878d1c9edcfc8b7503696c59
MarioRiera <ma.riera@duocuc.cl>
2021-07-13 21:42:20 -0400

    Solucion del problema anterior y creacion de forms.py

--- Prueba3/core/urls.py
+++ Prueba3/core/urls.py
@@ -1,12 +1,11 @@
-from Prueba3.api.views import formulario
+
 from django.urls import path
-from .views import home, inicio, galeria, nosotros, formulario
+from .views import home, inicio, galeria, nosotros
 
 urlpatterns = [
     path('',home, name="home"),
     path('index.html/',inicio, name="index"),
     path('galeria.html/',galeria, name="galeria"),
-    path('nosotros.html/',nosotros, name="nosotros"),
-    path('formularioBD.html/',formulario, name="formulario")
+    path('nosotros.html/',nosotros, name="nosotros")
     
 ]
